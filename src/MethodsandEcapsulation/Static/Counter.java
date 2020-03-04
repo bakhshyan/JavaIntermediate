aa4c922ca6093b77ed8cd2374cf32a5964f1d7ec
hayk.bakhshyan <haykbakhshyan@gmail.com>
2020-03-04 19:12:47 +0400

    OverloadingExample

--- src/MethodsandEcapsulation/Static/Counter.java
+++ src/MethodsandEcapsulation/Static/Counter.java
@@ -1,17 +1,28 @@
 package MethodsandEcapsulation.Static;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Counter {
 
 
-    public static void main(String[] args) throws Exception {
-        String strings[] = new String[]{"one", "two"};
-        List<String> list = Arrays.asList(strings);
-        System.out.println(list);
-
+    public static void main(String[] args) {
+        int number = 1;
+        String letters = "abc";
+        number(number);
+        letters = letters(letters);
+        System.out.println(number + letters);
     }
+
+    public static int number(int number) {
+        number++;
+        return number;
+    }
+
+
+    public static String letters(String letters) {
+        letters += "d";
+        return letters;
+    }
+
+
 }
 
 
