2cc74dc9754a7b695c8b4097bb191e227cd585d0
hayk.bakhshyan <haykbakhshyan@gmail.com>
2020-03-12 21:00:37 +0400

    Exceptions

---
+++ src/exceptions/Check.java
@@ -0,0 +1,24 @@
+package exceptions;
+
+public class Check {
+    void explore() {
+        try {
+            fall();
+        } catch (Exception e) {
+            System.out.println("get up");
+        }
+
+        finally {
+            System.out.println("Finally");
+        }
+
+    }
+
+    void fall()  {
+       // throw new RuntimeException();
+    }
+
+    public static void main(String[] args) {
+        new Check().explore();
+    }
+}
---
+++ src/exceptions/Main.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(new Main().myFunction(0));
+    }
+
+    Integer myFunction(Integer x) {
+        Integer y = 0;
+        if (x > 10) {
+            return 10000;
+        }
+
+        try {
+            //y = 10 / x;
+            return 10 / x;
+        } catch (Exception e) {
+            //y = 10;
+            return 10;
+        }finally {
+            //y = 200;
+            return 200;
+        }
+
+    }
+}
